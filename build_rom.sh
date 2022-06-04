791555575db6e45edf54c2665fd2c6b686af22c9
danascape <danascape@gmail.com>
2022-06-04 13:16:44 -0600

    rom: Push status updates to Telegram

--- build_rom.sh
+++ build_rom.sh
@@ -15,20 +15,32 @@ if [[ -z "$(which repo)" ]]; then
     exit 1
 fi
 
+# Set variables required for Telegram.
+API_KEY=""
+CHAT_ID=""
+
+# Setup necessary Telegram functions.
+function sendTG() {
+	curl -s "https://api.telegram.org/bot$API_KEY/sendmessage" --data "text=${*}&chat_id=$CHAT_ID&parse_mode=HTML" >/dev/null
+}
+
 # Sync the android source (depth sync since its a CI)
 if [[ -f "build/envsetup.sh" ]]; then
-    echo "Source already synced."
-    echo "Skipping Sync."
+    sendTG "Source already synced."
+    sendTG "Skipping Sync."
 else
+    sendTG "Syncing Source Code."
     repo init -u https://github.com/ProjectRobust/platform_manifest -b raijin --depth 1
     repo sync
 fi
 
 # Close QCOM SEPolicy
+sendTG "Cloning QCOM SEPolicy repos"
 git clone -b raijin https://github.com/ProjectRobust/device_qcom_sepolicy device/qcom/sepolicy
 git clone -b raijin https://github.com/ProjectRobust/device_qcom_sepolicy_vndr device/qcom/sepolicy_vndr
 
 # Clone device configuration repositories
+sendTG "Cloning device repositories"
 git clone -b raijin https://github.com/ProjectRobust/device_xiaomi_ginkgo device/xiaomi/ginkgo
 git clone -b raijin https://github.com/ProjectRobust/device_xiaomi_ginkgo-sepolicy device/xiaomi/ginkgo-sepolicy
 git clone -b raijin --depth 1 https://github.com/ProjectRobust/device_xiaomi_ginkgo-kernel device/xiaomi/ginkgo-kernel
@@ -36,6 +48,7 @@ git clone -b raijin --depth 1 https://github.com/ProjectRobust/kernel_xiaomi_gin
 git clone -b raijin --depth 1 https://github.com/ProjectRobust/vendor_xiaomi_ginkgo vendor/xiaomi/ginkgo
 
 # Start the build
+sendTG "Starting Build"
 source build/envsetup.sh
 lunch robust_ginkgo-userdebug
 m otapackage -j12
